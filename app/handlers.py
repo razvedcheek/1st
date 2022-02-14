2a9b2a905986ec2f512370cb8ac8f56fd7ab3c43
Pasha Maximov <pavel.maximov.04@mail.com>
2022-02-14 19:26:37 +0300

    heroku2

---
+++ app/handlers.py
@@ -0,0 +1,59 @@
+from flask import Flask
+import sqlalchemy
+from app import app
+from flask import (
+    make_response,
+    redirect,
+    render_template,
+    escape,
+    abort,
+    request,
+    session,
+    url_for,
+    flash,
+)
+from datetime import datetime, timedelta
+
+
+@app.errorhandler(404)
+def page_not_found(_):
+    return render_template("zaglushka.html"), 404
+
+
+@app.route('/')
+def mainpage():
+    return render_template('mainpage.html')
+
+
+@app.route('/mainpage.html')
+def mainpage1():
+    return render_template('mainpage.html')
+
+
+@app.route('/catalogpage.html')
+def catalogpage():
+    return render_template('catalogpage.html')
+
+
+@app.route('/zaglushka.html')
+def zaglushkapage():
+    return render_template('zaglushka.html')
+
+
+@app.route('/loginpage.html')
+def loginpage():
+    return render_template('loginpage.html')
+
+
+@app.route('/infopage.html')
+def infopage():
+    return render_template('infopage.html')
+
+@app.route('/good/<good>')
+def typepage(good):
+    t = good
+    return render_template('good.html', item = t)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
